6a4fa66ed2655bb8d2b9a1219018fc8e7a0a47d4
DESKTOP-TDSET4L\eduar <eduardo@daccord.com.br>
2024-03-20 18:28:02 -0300

    Refactors MainPresenter

--- WeddingApp/presenter/src/main/java/com/example/presenter/MainPresenter.kt
+++ WeddingApp/presenter/src/main/java/com/example/presenter/MainPresenter.kt
@@ -1,21 +1,47 @@
 package com.example.presenter
 
 import com.example.domain.interactors.ScheduleInteractor
-import com.example.domain.models.ScheduleModel
+import com.example.domain.models.EventModel
+import com.example.domain.utils.Outcome
 
 class MainPresenter(
     private var view: View?,
     private val scheduleDb: ScheduleInteractor,
 ) : BasePresenter() {
     interface View {
-        fun setSchedule(mainEvent:ScheduleModel, scheduleList: List<ScheduleModel>)
+        fun setMainEvent(mainEvent:EventModel)
+        fun setSchedule(scheduleList: List<EventModel>)
+    }
+
+    private companion object {
+        const val EVENT_ID = "wed-janet_barry"
     }
 
     override suspend fun onCreate() {
-        view?.setSchedule(
-            runInBackground { this.scheduleDb.getMainEvent() },
-            runInBackground { this.scheduleDb.getScheduleList() },
-        )
+
+        runInBackground {
+            val mainEvent = when (val scheduleRepositoryResult = this.scheduleDb.getMainEvent(EVENT_ID)) {
+                is Outcome.Success -> scheduleRepositoryResult.value
+                is Outcome.Failure -> {
+                    null
+                }
+            }
+
+            val scheduleList = when (val scheduleRepositoryResult = this.scheduleDb.getEventList(EVENT_ID)) {
+                is Outcome.Success -> scheduleRepositoryResult.value
+                is Outcome.Failure -> {
+                    null
+                }
+            }
+
+            scheduleList?.let {
+                enqueueOnMain { view?.setSchedule(it) }
+            }
+
+            mainEvent?.let {
+                enqueueOnMain { view?.setMainEvent(it) }
+            }
+        }
     }
 
     override suspend fun onStart() {}
